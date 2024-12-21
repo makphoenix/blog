b617564c22b7a4eaee43fa3218b256b6641ea8d9
Maddox Kashi <maddox.kashi@gmail.com>
2024-12-21 17:25:25 +0330

    page model rspecs

---
+++ spec/models/page_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe Page, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  subject { build(:page) }
+
+  # it "has a valid factory" do
+  #   expect(page).to be_valid
+  # end
+
+  describe "validations" do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_uniqueness_of(:title) }
+    it { is_expected.to validate_presence_of(:content) }
+  end
+
+  describe "#slug" do
+    let(:page) { create(:page, title: "--Foo Bar! _ 87 --") }
+    it "is generated from the title" do
+      expect(page.slug).to eq("foo-bar-87")
+    end
+  end
+end
