63e68fd2861437ebc1a2626621a15aec40575725
Maddox Kashi <maddox.kashi@gmail.com>
2024-12-21 17:28:25 +0330

    user model spec modification

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,9 +9,13 @@ RSpec.describe User, type: :model do
   end
 
   describe "validations" do
+    it { is_expected.to be_valid }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
     it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_uniqueness_of(:email) } 
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to allow_value("foo@bar.com").for(:email) }
+    it { is_expected.to_not allow_value("@bar.com").for(:email) }
+    it { is_expected.to_not allow_value("foo@").for(:email) }
   end
 end
